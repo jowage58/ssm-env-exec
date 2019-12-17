3faf9ae2e0e4d58b00e46d26748f8727694d24bb
John Wagenleitner <john.wagenleitner@gmail.com>
2019-12-17 08:10:18 -0800

    merge code to get rid of unneeded module

--- ssmenv2exec/__init__.py
+++ ssmenv2exec/__init__.py
@@ -1,10 +1,27 @@
 import logging
 import os
 import sys
+from typing import Mapping
 
-import ssmenv2exec.ssm
+import boto3
 
 logger = logging.getLogger('ssmenv2exec.main')
+
+
+def get_params_by_path(param_path: str, *, path_sep: str = '/') -> Mapping[str, str]:
+    """Returns a mapping of parameters under the given path."""
+    ssm_client = boto3.client('ssm')
+    params = ssm_client.get_parameters_by_path(
+        Path=param_path,
+        WithDecryption=True,
+    )
+    if not params['Parameters']:
+        logger.warning('No parameters found for path [%s]', param_path)
+        return {}
+    return {
+        param['Name'].rsplit(path_sep, maxsplit=1): param['Value']
+        for param in params['Parameters']
+    }
 
 
 def main() -> None:
@@ -12,10 +29,10 @@ def main() -> None:
         print('Usage:')
         print('\tssmenv2env /app/cas/ java -jar some.jar\n')
         sys.exit(1)
-    params = ssmenv2exec.ssm.get_params_by_path(sys.argv[1])
+    params = get_params_by_path(sys.argv[1])
     env = os.environ
     for k, v in params.items():
-        if not env.get(k):
+        if k not in env:
             env[k] = v
         else:
             logger.warning('Env var [%s] already exists, skipping', k)
