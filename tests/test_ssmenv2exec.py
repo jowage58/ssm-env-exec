b2d6d1443f78c3f84dff3784f864cb8c70a4c86f
John Wagenleitner <john.wagenleitner@gmail.com>
2019-12-18 08:04:55 -0800

    add TravisCI config add basic test

--- tests/test_ssmenv2exec.py
+++ tests/test_ssmenv2exec.py
@@ -11,3 +11,31 @@ class TestParameterParsing(unittest.TestCase):
 
     def test_parse_none(self):
         self.assertRaises(TypeError, ssmenv2exec.parse_ssm_params, None, path_sep='/')
+
+    def test_parse_params(self):
+        params = [
+            {'Name': '/app/myapp/DB_PASS',
+             'Type': 'String',
+             'Value': 'tiger',
+             },
+            {'Name': '/app/myapp/DB_URL',
+             'Type': 'String',
+             'Value': 'localhost/orcl',
+             },
+            {'Name': '/app/myapp/DB_USER',
+             'Type': 'String',
+             'Value': 'scott',
+             },
+            {'Name': '/app/myapp/SECRET_KEY',
+             'Type': 'SecureString',
+             'Value': 'super_secret',
+             }
+        ]
+        expected = {
+            'DB_PASS': 'tiger',
+            'DB_URL': 'localhost/orcl',
+            'DB_USER': 'scott',
+            'SECRET_KEY': 'super_secret'
+        }
+        actual = ssmenv2exec.parse_ssm_params(params, path_sep='/')
+        self.assertDictEqual(actual, expected)
